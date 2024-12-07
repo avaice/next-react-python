7c8403dd37825e8845a3ee67b139314bd405f0db
avaice <dev@cho-ice.xyz>
2024-12-07 18:06:01 +0900

    implement top page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
+import { ServerActionsSection } from "./_components/ServerActionsSection"
+import { StreamSection } from "./_components/StreamSection";
+
 export default function Home() {
+
+  
+
   return (
-    <div>
-      <h1>Next.js + Python</h1>
+    <div className="p-4 max-w-[800px] w-full mx-auto">
+      <header>
+      <h1 className="text-4xl">Next.js + Python</h1>
+      <p>Next.js+Pythonのテスト</p>
+      </header>
       
+      <div className="py-4 flex flex-col gap-2">
+        <ServerActionsSection />
+        <StreamSection />
+      </div>
     </div>
   );
 }
